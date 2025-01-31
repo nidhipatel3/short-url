ab9c6f2a3a96caa1d3db273b4374569393cc56e8
nidhipatel3 <jbsnidhipatel14@gmail.com>
2025-01-31 10:44:27 -0500

    Added Authentication JWT Authentication

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const path = require("path");
+const cookieParser = require("cookie-parser");
 const { connectToMongoDB } = require("./connect");
+const { restrictToLoggedinUserOnly, checkAuth } = require("./middlewares/auth");
+const URL = require("./models/url");
+
 const urlRoute = require("./routes/url");
 const staticRoute = require("./routes/staticRouter");
-const URL = require("./models/url");
+const userRoute = require("./routes/user");
 
 const app = express();
 const PORT = 8001;
@@ -17,6 +21,7 @@ app.set("views", path.resolve("./views"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 // app.get("/test", async (req, res) => {
 //     const allUrls = await URL.find({});
@@ -25,8 +30,9 @@ app.use(express.urlencoded({ extended: false }));
 //     });
 // });
 
-app.use("/url", urlRoute);
-app.use("/", staticRoute);
+app.use("/url", restrictToLoggedinUserOnly, urlRoute);
+app.use("/user", userRoute);
+app.use("/", checkAuth, staticRoute);
 
 app.get("/url/:shortId", async (req, res) => {
     const shortId = req.params.shortId;
---
+++ service/auth.js
@@ -0,0 +1,26 @@
+// const sessionIdToUserMap = new Map();
+const jwt = require("jsonwebtoken");
+const secret = "Abc$123@$";
+
+function setUser(user) {
+    // return sessionIdToUserMap.set(id, user);
+    return jwt.sign({
+        _id: user._id,
+        email: user.email,
+    }, secret);
+}
+
+function getUser(token) {
+    if (!token) return null;
+    // return sessionIdToUserMap.get(id);
+    try {
+        return jwt.verify(token, secret);
+    } catch (error) {
+        return null;
+    }
+}
+
+module.exports = {
+    setUser,
+    getUser,
+}
